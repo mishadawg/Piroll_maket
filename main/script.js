0a76287ecad3d7b21b3151745b933684bb5cbb66
Mike <->
2020-11-30 18:04:05 +0300

    style done.Script begin

---
+++ main/script.js
@@ -0,0 +1,14 @@
+let buttonLeft = document.querySelector('button_left')
+let buttonCentr = document.querySelector('button_centr')
+let buttonRight = document.querySelector('button_right')
+
+let quoteFirst = document.querySelector('quoteFirst').innerHTML;
+let quoteSecond = document.querySelector('quoteSecond').innerHTML;
+let quoteThird = document.querySelector('quoteThird').innerHTML;
+
+let charge = "0";
+
+buttonLeft.addEventListener('click'function(){
+    charge = "1";
+    
+})
