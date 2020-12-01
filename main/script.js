98bfc9b60b34459a3270497458c68d7832e4e192
Mike <->
2020-12-01 13:08:45 +0300

    js works,still needs ref and images

--- main/script.js
+++ main/script.js
@@ -1,14 +1,34 @@
-let buttonLeft = document.querySelector('button_left')
-let buttonCentr = document.querySelector('button_centr')
-let buttonRight = document.querySelector('button_right')
+let buttonLeft = document.querySelector('#button_left');
+let buttonCentr = document.querySelector('#button_centr');
+let buttonRight = document.querySelector('#button_right');
+let buttonPlay = document.querySelector('.play_button');
 
-let quoteFirst = document.querySelector('quoteFirst').innerHTML;
-let quoteSecond = document.querySelector('quoteSecond').innerHTML;
-let quoteThird = document.querySelector('quoteThird').innerHTML;
+let quoteFirst = document.querySelector('#quoteFirst');
+let quoteSecond = document.querySelector('#quoteSecond');
+let quoteThird = document.querySelector('#quoteThird');
 
-let charge = "0";
+let video = document.querySelector('.video');
 
-buttonLeft.addEventListener('click'function(){
-    charge = "1";
-    
-})
+buttonPlay.addEventListener('click',function(){
+    console.log(`click!`)
+    if(video.paused){
+        video.play();
+    }else{
+        video.pause();
+    }
+});
+buttonLeft.addEventListener('click',function(){
+    quoteFirst.classList.add('block')
+    quoteSecond.classList.remove('block')
+    quoteThird.classList.remove('block')
+});
+buttonCentr.addEventListener('click',function(){
+    quoteFirst.classList.remove('block')
+    quoteSecond.classList.add('block')
+    quoteThird.classList.remove('block')
+});
+buttonRight.addEventListener('click',function(){
+    quoteFirst.classList.remove('block')
+    quoteSecond.classList.remove('block')
+    quoteThird.classList.add('block')
+});
